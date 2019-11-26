8506071c8553f7ce29fafe21968bb1fe6062d244
Creator <sergej.kosov@gmail.com>
2019-11-26 13:48:06 +0100

    Solution to 4.2

--- src/ShaderPhongBumpMapped.h
+++ src/ShaderPhongBumpMapped.h
@@ -30,8 +30,66 @@ public:
   
 	virtual Vec3f Shade(const Ray& ray) const override
 	{
-		// --- PUT YOUR CODE HERE ---
-		return RGB(0,0,0);
+		// get shading normal
+		Vec3f normal = ray.hit->getNormal(ray);
+
+		// bump-mapping
+		Vec3f hitpoint = ray.org + ray.t * ray.dir;
+		float du = 0.5f * cosf(3 * hitpoint.val[0] * sinf(hitpoint.val[2]));
+		float dv = 0.5f * sinf(13 * hitpoint.val[2]);
+		normal += du * Vec3f(1, 0, 0) + dv * Vec3f(0, 0, 1);
+		normal = normalize(normal);
+		
+		// turn normal to front
+		if (normal.dot(ray.dir) > 0)
+			normal = -normal;
+
+		// calculate reflection vector
+		Vec3f reflect = normalize(ray.dir - 2 * normal.dot(ray.dir) * normal);
+
+		// ambient term
+		Vec3f ambientIntensity(1,1,1);
+
+		Vec3f color = CShaderFlat::Shade();
+		Vec3f ambientColor = m_ka * color;
+		Vec3f res = ambientColor.mul(ambientIntensity);
+
+		// shadow ray (up to now only for the light direction)
+		Ray shadow;
+		shadow.org = ray.org + ray.t * ray.dir;
+
+		// iterate over all light sources
+		for (auto pLight : m_scene.m_vpLights)
+			for(int s = 0; s < nAreaSamples; s++) {	// TODO: make the sampling to depend on the light source type
+				// get direction to light, and intensity
+				std::optional<Vec3f> lightIntensity = pLight->Illuminate(shadow);
+				if (lightIntensity) {
+					// diffuse term
+					float cosLightNormal = shadow.dir.dot(normal);
+					if (cosLightNormal > 0) {
+						if (m_scene.Occluded(shadow))
+							continue;
+
+						Vec3f diffuseColor = m_kd * color;
+						res += (diffuseColor * cosLightNormal).mul(lightIntensity.value());
+					}
+
+					// specular term
+					float cosLightReflect = shadow.dir.dot(reflect);
+					if (cosLightReflect > 0) {
+						Vec3f specularColor = m_ks * RGB(1, 1, 1); // white highlight;
+						res += (specularColor * powf(cosLightReflect, m_ke)).mul(lightIntensity.value());
+					}
+				}
+			}
+
+		if (nAreaSamples > 1)
+			res /= nAreaSamples;
+
+		for (int i = 0; i < 3; i++)
+			if (res.val[i] > 1) res.val[i] = 1;
+		
+		return res;
 	}
 
 private:

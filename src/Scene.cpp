ed4a96320fe5070619f7a2214a7daf46ca3cead8
Creator <sergej.kosov@gmail.com>
2019-12-28 21:39:42 +0100

    Last changes for the task

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -17,7 +17,7 @@ void CScene::ParseOBJ(const std::string& fileName)
 	if (file.is_open()) {
 		std::cout << "Parsing OBJFile : " << fileName << std::endl;
 
-		std::shared_ptr<IShader> pShader = std::make_shared<CShaderEyelightTextured>(RGB(1, 0.5f, 0), "../../../data/cb.bmp");
+		std::shared_ptr<IShader> pShader = std::make_shared<CShaderPhong>(*this, RGB(1, 1, 1), 0.5f, 0.5f, 0, 0);
 		std::vector<Vec3f> vVertexes;
 		std::vector<Vec3f> vNormals;
 		std::vector<Vec2f> vTextures;
--- src/main.cpp
+++ src/main.cpp
@@ -24,38 +24,22 @@ Mat RenderFrame(void)
 	// Define a scene
 	CScene scene;
 
-	// Load scene description 
-//	scene.ParseOBJ("../../../data/cone32.obj");
-//	scene.ParseOBJ("../../../data/barney.obj");
-	scene.ParseOBJ("../../../data/ground.obj");
+	auto pShaderWhite	= std::make_shared<CShaderPhong>(scene, RGB(1, 1, 1), 0.5f, 0.5f, 0, 0);
+	auto pShaderRed		= std::make_shared<CShaderPhong>(scene, RGB(1, 0, 0), 0.5f, 0.5f, 0, 0);
+	
+	// Load scene description
+	scene.ParseOBJ("../../../data/Torus Knot.obj");
 
+	scene.Add(std::make_shared<CPrimTriangle>(Vec3f(0, 0, 50), Vec3f(0, 0, -50), Vec3f(-50, 0, -50), pShaderWhite));
+	scene.Add(std::make_shared<CPrimTriangle>(Vec3f(0, 0, -50), Vec3f(0, 0, 50), Vec3f(50, 0, 50), pShaderRed));
+	
+	scene.Add(std::make_shared<CLightPoint>(Vec3f::all(5), Vec3f(0, 4, 10)));
+	scene.Add(std::make_shared<CLightPoint>(Vec3f::all(5), Vec3f(0, 10, 0)));
+	
 #ifdef ENABLE_BSP
 	// Build BSPTree
 	scene.BuildAccelStructure();
 #endif
-
-	// --- Scene description for 4.2 only ---
-
-//	auto shd1 = std::make_shared<CShaderPhongBumpMapped>(scene, RGB(1, 0, 0), 0.1f, 0.5f, 0.5f, 40); // red surface
-//	auto shd2 = std::make_shared<CShaderPhongBumpMapped>(scene, RGB(1, 1, 0), 0.1f, 0.5f, 0.5f, 40); // yellow surface
-//
-//	auto shd3 = std::make_shared<CShaderPhong>(scene, RGB(0, 1, 1), 0.1f, 0.5f, 0.5f, 40); // cyan surface
-//	auto shd4 = std::make_shared<CShaderPhong>(scene, RGB(0, 0, 1), 0.1f, 0.5f, 0.5f, 40); // blue surface
-//
-//	scene.Add(std::make_shared<CPrimSphere>(Vec3f(-2, 1.7f, 0), 2, shd1));
-//	scene.Add(std::make_shared<CPrimSphere>(Vec3f(1, -1, 1), 2.2f, shd3));
-//	scene.Add(std::make_shared<CPrimSphere>(Vec3f(3, 0.8f, -2), 2, shd4));
-//	scene.Add(std::make_shared<CPrimPlane>(Vec3f(0, -1, 0), Vec3f(0, 1, 0), shd2));
-//
-//	Vec3f pointLightIntensity(7, 7, 7);
-//	Vec3f lightPosition2(-3, 5, 4);
-//	Vec3f lightPosition3(0, 1, 4);
-//
-//	scene.Add(std::make_shared<CLightPoint>(pointLightIntensity, lightPosition2));
-//	scene.Add(std::make_shared<CLightPoint>(pointLightIntensity, lightPosition3));
-
-	// --- End description for 4.2 ---
-
 
 	Mat img(scene.m_pCamera->getResolution(), CV_32FC3);		// image array
 	Ray ray;                                          			// primary ray
